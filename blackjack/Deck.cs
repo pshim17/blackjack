714ac3db495bfe1effeb7c574f33527f68477f62
Patrick Shim <patrick.shim91@yahoo.com>
2025-08-27 11:35:15 -0400

    Fix back to full 52 card deck: Was altered for testing purposes

--- blackjack/Deck.cs
+++ blackjack/Deck.cs
@@ -15,8 +15,7 @@ namespace blackjack
             cards = new List<Card>();
 
             string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
-            //string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-            string[] ranks = { "2", "3", "A" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
 
             foreach (string suit in suits)
             {
