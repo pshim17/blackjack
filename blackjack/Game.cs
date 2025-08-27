288b03967f691562e0a22a5445c1329b67310c37
Patrick Shim <patrick.shim91@yahoo.com>
2025-08-27 14:17:02 -0400

    Refactored HitOrStand method: Move Hit and Stand into separate methods

--- blackjack/Game.cs
+++ blackjack/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +30,7 @@ namespace blackjack
             Console.WriteLine($"Dealer drew: {secondCardDealer.Rank} of {secondCardDealer.Suit}");
             Console.WriteLine($"Dealer's visible card total: {secondCardDealer.Value}\n");
 
+            Outcome();
             HitOrStand();
             Outcome();
         }
@@ -43,11 +45,8 @@ namespace blackjack
 
                 if (userInput == "h")
                 {
-                    Card playerCard = player.DrawCardFromDeck(deck);
+                    Hit();
 
-                    Console.WriteLine($"\nYou drew: {playerCard.Rank} of {playerCard.Suit}");
-                    Console.WriteLine($"Your current total is: {player.Hand.TotalCardValue()}\n");
-                    
                     if (player.Hand.TotalCardValue() > 21)
                     {
                         return;
@@ -55,17 +54,7 @@ namespace blackjack
                 }
                 else if (userInput == "s")
                 {
-                    Card dealerHiddenCard = dealer.Hand.GetDealerHiddenCard();
-
-                    Console.WriteLine($"\nDealer's hidden card was: {dealerHiddenCard.Rank} of {dealerHiddenCard.Suit}\n");
-
-                    while (dealer.Hand.TotalCardValue() < 17)
-                    {
-                        Card dealerCard = dealer.DrawCardFromDeck(deck);
-                        Console.WriteLine($"Dealer drew: {dealerCard.Rank} of {dealerCard.Suit}");
-                    }
-
-                    Console.WriteLine($"Dealer final total: {dealer.Hand.TotalCardValue()}\n");
+                    Stand();
                     return;
                 }
                 else
@@ -129,6 +118,29 @@ namespace blackjack
                 Console.WriteLine($"  - {cardRank} of {cardSuit}");
             }
             Console.WriteLine($"Total: {dealerTotal}\n");
+        }
+
+        public void Hit()
+        {
+            Card playerCard = player.DrawCardFromDeck(deck);
+
+            Console.WriteLine($"\nYou drew: {playerCard.Rank} of {playerCard.Suit}");
+            Console.WriteLine($"Your current total is: {player.Hand.TotalCardValue()}\n");
+        }
+
+        public void Stand()
+        {
+            Card dealerHiddenCard = dealer.Hand.GetDealerHiddenCard();
+
+            Console.WriteLine($"\nDealer's hidden card was: {dealerHiddenCard.Rank} of {dealerHiddenCard.Suit}\n");
+
+            while (dealer.Hand.TotalCardValue() < 17)
+            {
+                Card dealerCard = dealer.DrawCardFromDeck(deck);
+                Console.WriteLine($"Dealer drew: {dealerCard.Rank} of {dealerCard.Suit}");
+            }
+
+            Console.WriteLine($"Dealer final total: {dealer.Hand.TotalCardValue()}\n");
         }
     }
 }
